d1d3d3f2253846f8d210bf4597f2de349cac79b8
kjablonski08@gmail.com <kjablonski08@gmail.com>
2021-02-08 16:04:22 -0500

    added chakra ui and search component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+	Stack,
+	Input,
+	InputGroup,
+	InputLeftElement,
+	Container,
+	Button,
+} from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
+
+const SearchBar = () => {
+	return (
+		<div>
+			<Container>
+				<Stack spacing={4}>
+					<InputGroup>
+						<InputLeftElement
+							pointerEvents='none'
+							children={<SearchIcon color='gray.300' />}
+						/>
+						<Input
+							focusBorderColor='lime'
+							type='search'
+							name='search'
+							placeholder='Search'
+						/>
+					</InputGroup>
+				</Stack>
+			</Container>
+		</div>
+	);
+};
+
+export default SearchBar;

e3682ffc7d3396f50858c47fd59ab982aa52e864
kjablonski08@gmail.com <kjablonski08@gmail.com>
2021-02-08 23:48:27 -0500

    height and width still need work but photos displaying as expected

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -4,7 +4,6 @@ import Gallery from 'react-photo-gallery';
 import Carousel, { Modal, ModalGateway } from 'react-images';
 
 const PhotoGallery = ({ query }) => {
-	const API_KEY = process.env.REACT_APP_FLICKR_API_KEY;
 	const [photos, setPhotos] = useState(null);
 	const [currentImage, setCurrentImage] = useState(0);
 	const [viewerIsOpen, setViewerIsOpen] = useState(false);
@@ -20,6 +19,7 @@ const PhotoGallery = ({ query }) => {
 	};
 
 	useEffect(() => {
+		const API_KEY = process.env.REACT_APP_FLICKR_API_KEY;
 		const flickr = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=${query}&per_page=20&format=json&nojsoncallback=1`;
 		axios
 			.get(flickr)
@@ -29,18 +29,10 @@ const PhotoGallery = ({ query }) => {
 				pics.map((pic) => {
 					let photoObj = {
 						src: `https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`,
-						width: '',
-						height: '',
+						width: 80,
+						height: 80,
 					};
-					axios
-						.get(
-							`https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${API_KEY}&photo_id=${pic.id}&format=json&nojsoncallback=1`
-						)
-						.then((res) => {
-							photoObj.height = res.data.sizes.size[6].height;
-							photoObj.width = res.data.sizes.size[6].width;
-						})
-						.then(photoObjs.push(photoObj));
+					photoObjs.push(photoObj);
 				});
 				setPhotos(photoObjs);
 			})
@@ -55,7 +47,6 @@ const PhotoGallery = ({ query }) => {
 	}
 	return (
 		<div className='gallery-container horizontal-spacing'>
-			<h1>Results</h1>
 			<Gallery photos={photos} onClick={openLightbox} />
 			<ModalGateway>
 				{viewerIsOpen ? (
